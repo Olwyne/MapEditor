fb3e59919c5dee4b3d34b9b038beb8b5a7f177ea
Monica <monicalisacek@hotmail.com>
2020-01-02 13:54:02 +0100

    doxgen camera.hpp + cube.hpp

--- glimac/include/glimac/Camera.hpp
+++ glimac/include/glimac/Camera.hpp
@@ -2,16 +2,26 @@
 #include <glimac/SDLWindowManager.hpp>
 
 
-/* asbtract class */
+/// \class Camera
+/// \brief abstract class containingFreeFly and TrackBall cameras' methods' declarations
 class Camera
 {
  public:
 
+    /// \brief the camera gets closer
+    /// \param delta : coefficient
     virtual void moveFront(float delta)=0;
+    /// \brief rotate left
+    /// \param degrees : angle of rotation
     virtual void rotateLeft (float degrees)=0;
+    /// \brief rotate up
+    /// \param degrees : angle of rotation
     virtual void rotateUp(float degrees)=0;
+    /// \brief moves the camera in function of the keys pressed
+    /// \param e : SDL_event which tells us what key is being pressed
     virtual void move_camera_key_pressed(const SDL_Event &e)=0;
 
+    /// \brief return the View Matrix of th camera
     virtual glm::mat4 getViewMatrix() const=0;
 
 };
--- glimac/include/glimac/Cube.hpp
+++ glimac/include/glimac/Cube.hpp
@@ -9,27 +9,41 @@ using namespace glimac;
 class Cube: public Object
 {
 private:
-  bool m_invisible;
-  unsigned int m_type;
+  bool m_invisible; /*!< Visibility of the cube */
+  unsigned int m_type; /*!< Type of the cube */
 
 public:
+	/// \brief constructor that sets the cube to invisible of type 0
   Cube() : Object(36), m_invisible(1), m_type(0) {}
+  /// \brief constructor that uses Param_Pos_Color to set the cube to visible, and set its position and color
   Cube(const Param_Pos_Color param) 
          : Object(param, 36), m_invisible(0), m_type(0) {}
-
+  /// \brief reference constructor
   Cube(const Cube &c) : Object(c), m_invisible(c.m_invisible) {}
   ~Cube() override = default;
 
+	/// \brief defined in Object.hpp: adapted to a cube
   void create_vbo_vao(bool scene_modified) override;
+  /// \brief set the cube's color
+	/// \param color : color to set
   void set_color(const glm::vec3 color);
+  /// \brief set the cube's visibility
+	/// \param invisible : bool, if true the cube will be set to invisible
   void set_invisible(const bool invisible);
+  /// \brief set the cube's type
+	/// \param color : type to set
   glm::vec3 set_type(unsigned int type);
 
-  //need to be defined so that vector related functions work
+  /// \brief overloading of paramter '='
+	/// \param c : cube 
   Cube& operator=(const Cube& c);
+  /// \brief overloading of paramter '=='
+	/// \param c : cube 
   bool operator==(const Cube& c);
   
+  /// \brief returns true if the cube is invisible, false if not
   inline bool is_invisible() { return m_invisible; }
+  /// \brief returns the type of the cube
   inline unsigned int get_type() { return m_type; }
   
 };
